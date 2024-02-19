bf4d81a13d152e1e34206b1fc4003e9aa75ff79b
Ceciliapradob <ceciliapradob0799@gmail.com>
2024-02-18 21:57:04 -0300

    Culminados

---
+++ app.js
@@ -0,0 +1,63 @@
+// Juego de ahorcado:
+
+// La computadora selecciona una palabra aleatoria.
+// El usuario tiene un número limitado de intentos para adivinar la palabra.
+// Después de cada intento, muestra las letras correctas adivinadas hasta el momento.
+
+//--------------------------Ejercicio 2------------------------------
+// Generador de contraseñas:
+
+// Solicita al usuario la longitud deseada de la contraseña.
+// Combina letras, números y caracteres especiales de forma aleatoria para generar la contraseña.
+
+//--------------------------Ejercicio 3------------------------------
+// Calculadora de propinas:
+
+// Pide al usuario ingresar el monto total de la factura.
+// Solicita el porcentaje de propina deseado.
+// Calcula y muestra el monto total incluyendo la propina.
+
+//--------------------------Ejercicio 4------------------------------
+// Comprobador de palíndromos:
+
+// De una frase o palabra existente.
+// Comprueba si es un palíndromo y muestra el resultado.
+
+//--------------------------Ejercicio 5------------------------------
+// Ordenar números:
+
+// A una función ingresa una lista de números.
+// Le permite elegir entre ordenar la lista de manera ascendente o descendente.
+
+
+//--------------------------Ejercicio 6------------------------------
+// Juego de piedra, papel o tijera:
+
+// Permite al usuario elegir entre piedra, papel o tijera.
+// La computadora selecciona aleatoriamente una opción.
+// Determina el ganador según las reglas del juego.
+
+//--------------------------Ejercicio 7------------------------------
+// Contador de vocales:
+
+// Solicita al usuario ingresar una palabra o frase.
+// Cuenta y muestra la cantidad de vocales en la entrada.
+
+//--------------------------Ejercicio 8------------------------------
+// Convertidor de unidades:
+
+// Ingresa un valor y la unidad de origen.
+// Permite elegir la unidad de destino y muestra el resultado de la conversión.
+
+//--------------------------Ejercicio 9------------------------------
+// Número primo:
+
+// De un número existente,
+// determina si el número es primo y muestra el resultado.
+
+//--------------------------Ejercicio 10------------------------------
+// Juego de adivinanzas:
+
+// La computadora selecciona un número aleatorio.
+// El usuario tiene que adivinar el número.
+// Después de cada intento, proporciona pistas sobre si el número es demasiado alto o demasiado bajo.
